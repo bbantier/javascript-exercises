d2eacf73b58de6cbb411c9c7be6d6e859cbaa876
bbantier <65014410+bbantier@users.noreply.github.com>
2024-11-12 16:56:37 +0100

    complete js 10 and pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (position) {
+  const fibonacciArray = [0, 1];
 
+  if (position < 0) return "OOPS";
+
+  while (fibonacciArray.length < position + 1) {
+    fibonacciArray.push(
+      fibonacciArray[fibonacciArray.length - 1] +
+        fibonacciArray[fibonacciArray.length - 2]
+    );
+  }
+  return fibonacciArray[position];
 };
 
 // Do not edit below this line
