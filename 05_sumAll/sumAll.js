f91a6a446806ad59a7551225af65eed5c21b8eaa
bbantier <65014410+bbantier@users.noreply.github.com>
2024-11-12 11:24:41 +0100

    complete js 05 and pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (startNumber, endNumber) {
+  let sum = 0;
+  const smallerNumber = Math.min(startNumber, endNumber);
+  const largerNumber = Math.max(startNumber, endNumber);
 
+  if (
+    startNumber < 0 ||
+    endNumber < 0 ||
+    !Number.isInteger(startNumber) ||
+    !Number.isInteger(endNumber)
+  ) {
+    return "ERROR";
+  }
+
+  for (let i = smallerNumber; i <= largerNumber; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
