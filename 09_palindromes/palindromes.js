0d70d55dfcf6220c9f38a4bf00d70a318d9c3ee6
bbantier <65014410+bbantier@users.noreply.github.com>
2024-11-12 16:18:31 +0100

    complete js 09 and pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const regex = /[^(A-Za-z0-9)]/g;
+  const cleanString = string
+    .replace(regex, "")
+    .toLowerCase();
+  const reversedString = cleanString
+    .split("")
+    .reverse()
+    .join("");
 
+  return cleanString === reversedString;
 };
 
 // Do not edit below this line
