9e3afb3432ad00cf5d4f642acba123943523b681
bbantier <65014410+bbantier@users.noreply.github.com>
2024-11-12 10:57:44 +0100

    complete js 04 and pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...values) {
+  const newArray = [];
+  array.forEach((el) => {
+    if (!values.includes(el)) {
+      newArray.push(el);
+    }
+  })
+  return newArray;
 };
 
 // Do not edit below this line
