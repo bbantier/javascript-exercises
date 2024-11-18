031684c52d41a92cc57391ee0aa17db2d13daf44
bbantier <65014410+bbantier@users.noreply.github.com>
2024-11-18 14:49:33 +0100

    complete js 12 and pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+  return array.sort((a, b) => {
+    const lastAge =
+      (!a.yearOfDeath ? new Date().getFullYear() : a.yearOfDeath) -
+      a.yearOfBirth;
+    const nextAge =
+      (!b.yearOfDeath ? new Date().getFullYear() : b.yearOfDeath) -
+      b.yearOfBirth;
+    return nextAge - lastAge;
+  })[0];
 };
 
 // Do not edit below this line
